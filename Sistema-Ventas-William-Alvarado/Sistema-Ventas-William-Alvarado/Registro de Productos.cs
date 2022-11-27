29789aec102ccb24b04a20641a2fa94fe7585329
William Alvarado <115381750+WilliamAlx22@users.noreply.github.com>
2022-11-27 06:32:03 -0600

    Add files via upload

---
+++ Sistema-Ventas-William-Alvarado/Sistema-Ventas-William-Alvarado/Registro de Productos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Ventas_William_Alvarado
+{
+    public partial class Registro_de_Productos : Form
+    {
+        public Registro_de_Productos()
+        {
+            InitializeComponent();
+        }
+    }
+}
